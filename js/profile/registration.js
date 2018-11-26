0071703b2ec1aa1a316306921db61461003d6082
neethuisep <neethus.061@gmail.com>
2018-11-27 00:23:58 +0100

    Registration Page
    Registration - html, jquery and php code first version

--- js/profile/registration.js
+++ js/profile/registration.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
     console.log("register");
+	
+	
 });
+
+function validateFields(){
+	if(!$('#fname').val()){
+		alert("Please enter the first name");
+	}
+	if($('#fname').val()&& !$('#lname').val()){
+		alert("Please enter the last name");
+	}
+	if($('#fname').val() && $('#lname').val() && !$('#email').val()){
+		alert("Please enter the email id");
+	}
+	
+	if($('#fname').val() && $('#lname').val() && $('#email').val() && $('#email').val() && !$('#password').val()){
+		alert("Please enter the password");
+	}
+		if($('#fname').val() && $('#lname').val() && $('#email').val() && $('#email').val() && $('#password').val() && !$('#cpassword').val()){
+		alert("Please confirm the password");
+	}
+	
+		if(($('#password').val() && $('#cpassword').val()) && ($('#cpassword').val() != ($('#cpassword').val()))){
+		alert("Passwords enterd do not match");
+	}
+	
+	
+}
+
+
+function reset(){
+	$('#fname').val()='';
+	
+	
+}
