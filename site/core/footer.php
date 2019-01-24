0c84eb0b728f508be86815ccaf485bd90eaf9e06
Krishna123isep <45043987+Krishna123isep@users.noreply.github.com>
2019-01-24 12:23:38 +0100

    Update site/core/footer.php

--- site/core/footer.php
+++ site/core/footer.php
@@ -7,17 +7,17 @@
 
 <div class="footer-social-icons">
 <ul>
-<li><a href="#" target="blank"><i class="fa fa-facebook"></i></a></li>
-<li><a href="#" target="blank"><i class="fa fa-twitter"></i></a></li>
-<li><a href="#" target="blank"><i class="fa fa-google-plus"></i></a></li>
-<li><a href="#" target="blank"><i class="fa fa-instagram"></i></a></li>
+<li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+<li><a href="https://twitter.com/Twitter?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"><i class="fa fa-twitter"></i></a></li>
+<li><a href="https://plus.google.com/discover" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+<li><a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram"></i></a></li>
 </ul>
 </div>
 
 <div class="footer-menu-one">
 <ul>
-<li><a href="#">Home</a></li>
-<li><a href="#">Contact us</a></li>
+<li><a href="/smarthome/site/unauthorized/home.php">Home</a></li>
+<li><a href="/smarthome/site/user/contact-us.php">Contact us</a></li>
 </ul>
 </div>
 <div class="footer-menu-two">
