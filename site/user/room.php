674de78dfb874221e66bc28e59d5e590106e81e8
neethuisep <neethus.061@gmail.com>
2019-01-24 14:04:08 +0100

    change

--- site/user/room.php
+++ site/user/room.php
@@ -1,10 +1,23 @@
+
+<?php
+//echo "here==>";
+	 session_start();
+	  $now = time(); // Checking the time now when home page starts.
+	   include("../core/header.php");
+	if( isset($_SESSION['user']) && ! ($now > $_SESSION['expire']))
+	{
+		 //session_start();
+
+ ?>
+
+
 <link href="../../bootstrap/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="../../bootstrap/js/bootstrap.min.js"></script>
 <script src="../../js/jquery/jquery.min.js"></script>
 
 <!------ Include the above in your HEAD tag ---------->
 <link href="../../css/registration.css" rel="stylesheet" id="bootstrap-css">
-<link href="../../css/menu.css" rel="stylesheet" id="bootstrap-css">
+<script src="../../css/utility.css" rel="stylesheet" id="bootstrap-css"></script>
 <script type="text/javascript">
 
 
@@ -17,6 +30,7 @@
      <title>Add Room</title>
      	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+
 <body>
 <?php
    include("../core/loginmenu.php");
@@ -24,31 +38,38 @@
 <?php
   include("../core/dropdownmenu.php");
   
-?>	
+?>
 
  <div class="container" >
  <!---heading---->
      <header class="heading"> Add Room </header>
 	 <div class = "space"></div>
 	<!---Form starting----> 
-	<!--<form method="post" name="" >-->
+	<form method="post" name="" >
 	<div class="row ">
 	
 	 <!--- For Name---->
          <div class="col-sm-12">
              <div class="row">
+			 <div class="col-sm-12">
+             <div class="row">
+			     <div class="col-xs-12">
+          	         <label class="error hidden" id="error" style="color:red;"></label> </div>
+		      </div>
+		 </div>
+			 
 			     <div class="col-xs-4">
           	         <label class="firstname">Apartment Name </label> </div>
 		         <div class="col-xs-8">
 		            <?php
 					include("../../config/config.php");
-					session_start();
+					
 					$sql="SELECT id,name FROM apartment order by name"; 
 
 
 
 					echo "<select name= 'apname' id='apname' style= 'width:300px;height:35px;'>"; // list box select command
-					echo "<option selected=selected' value='select room name'>select room name</option>";
+					echo "<option selected=selected' value=''>select apartment name</option>";
 						foreach ($db->query($sql) as $row){//Array or records stored in $row
 							//echo 'value is : '.$row['name'];
 							echo "<option value='$row[id]'>".$row['name']."</option>"; 
@@ -94,15 +115,10 @@
 		
 		     <div class="col-sm-12">
 		         <div  >
-				<!-- <input class="btnsubmit btn-warning submit"  name="add_room_submit" id="add_room_submit" type="submit" value=
-                            "Add Another Room" onclick=validateRoomFields()>-->
+				 
 							
-							<input class="btnsubmit btn-warning submit"  name="add_sensor_submit" id="add_sensor_submit" type="submit" value="Next" onclick=NextvalidateRoomFields()
-                             />
-							 
-							 <input class="btnsubmit btn-warning submit"  name="add_sensor_submit" id="add_sensor_submit" type="button" value="Cancel" onclick=NextvalidateRoomFields()
-                             />
-							 </div>
+							<input class="btnsubmit btn-warning submit"  name="add_room_submit" id="add_room_submit" type="submit" value="Next" 
+                             ></div>
 							
 							
 				 
@@ -110,7 +126,7 @@
 		 </div>
 		 
 	 </div>	 
-	<!--</form>	--> 		 
+	</form>	 		 
 		 
 </div>
 
@@ -118,6 +134,15 @@
 </body>
 	<script src="../../js/profile/room.js"></script>
 </html>
+<?php
+	}else{
+		session_destroy();
+		//die();
+		//echo "<script language='javascript' type='text/javascript'> location.href='/smarthome/site/unauthorized/login.php' </script>";   
+		 header("Location: http://localhost/smarthome/site/unauthorized/login.php");
+		 exit();
+	}
 
+?>
  
 	
