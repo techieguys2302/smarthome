0fa355af1afda717554050c72e658ea6f533f107
Saurabh Pareek <pareeksp47@gmail.com>
2019-01-24 11:04:47 +0100

    changes
    Contact us - 70% done by suraj and 30% saurabh

--- site/core/menu.php
+++ site/core/menu.php
@@ -1,6 +1,6 @@
 
 <link href="../../css/menu.css" rel="stylesheet" id="bootstrap-css">
-<div class="menu" >
+<div class="menu loggedout" >
 <div class="leftmenu">
 <!--<img class="icon" src="../../images/icon.png" />-->
 <h4><a href="/smarthome/site/unauthorized/home.php" class="homeicon"><img class="icon" src="../../images/icon.png" />&nbsp;&nbsp;<label class="domisep">DOMISEP</label> <label class="smart">SMART-HOME</label></a></h4>
--- site/user/contact-us.php
+++ site/user/contact-us.php
@@ -1,35 +1,49 @@
 <?php
+//echo "here==>";
+	 session_start();
+	  $now = time(); // Checking the time now when home page starts.
+	   include("../core/header.php");
+	if( isset($_SESSION['user']) && ! ($now > $_SESSION['expire']))
+	{
+		 //session_start();
+		 $_SESSION['expire'] = time() + (30 * 60);
+
+ ?>
+
+<?php
 
 $result="";
 
 if(isset($_POST['submit'])){
-	require 'phpmailer/PHPMailerAutoload.php';
-	$mail = new PHPMailer;
+	require ('../../../PHPMailer_5.2.4/class.phpmailer.php');
+	$mail = new PHPMailer();
 	
-	
-	
+	$mail->IsSMTP();
+	//$mail->SMTPDebug=2;
 	$mail->Host='smtp.gmail.com';
-	$mail->Port=587;
+	$mail->Port=465;
 	$mail->SMTPAuth=true;
-	$mail->SMTPSecure='tls';
-	$mail->Username='krishnapeapalla1996@gmail.com';
-	$mail->Password='france2018';
+	$mail->SMTPSecure='ssl';
+	$mail->Username='techieguys2302@gmail.com';
+	$mail->Password='Techie@123';
 	
-	$mail->setFrom($_POST['email'],$_POST['name']);
-	$mail->addAddress('surajreddy81688@gmail.com');
-	$mail->addReplyTo($_POST['email'],$_POST['name']);
+	$mail->SetFrom($_POST['email'],$_POST['name']);
+	$mail->AddAddress('techieguys2302@gmail.com');
+	$mail->AddReplyTo($_POST['email'],$_POST['name']);
 	
-	$mail->isHTML(true);
-	$mail->Subject='Form Submission: '.$_POST['subject'];
-	$mail->Body='<h1 align=center>Name:'.$_POST['name'].'<br>Email: '.$_POST['email'].'<br
-	>Message: '.$_POST['msg'].'</h1>';
+	$mail->IsHTML(true);
+	$mail->Subject='Smarthome User Enquiry: '.$_POST['subject'];
+	$mail->Body='<h1 align=center>Name:'.$_POST['name'].'<br/><br/>Email: '.$_POST['email'].'<br/
+	></h1><br/><br/><h4>Message: '.$_POST['msg'].'<h4/>';
 	
 	if(!$mail->send()){
-		$result="something went wrong. Please try again.";
+		$result="Something went wrong. Please try again.";
 	}
 	else{
-		$result="thanks".$_POST['name']." for contacting us. We'll get back to you soon!";
-		
+		$result="Thanks ".$_POST['name']." for contacting us. We'll get back to you soon!";
+		$description = 'Name : '.$_POST['name'].'\<br\/\>Subject: '.$_POST['subject'].'\<br\/\> Description: '.$_POST['msg'];
+		include("../../config/config.php");
+		mysqli_query($db, "Insert into complaint(Description,User_Id,Date) values ('".$description."','" .$_SESSION['user'][0]['ID']. "',NOW()); ");
 	}
 	
 	
@@ -43,18 +57,26 @@ if(isset($_POST['submit'])){
 
 <head>
   <meta charset="UTF-8">
-  <meta name="author" content="Sahil Kumar">
+  <meta name="author" content="">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Contact Form Bootstrap</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+  <link rel="stylesheet" href="../../css/contact-us.css">
 </head>
 
 <body class="bg-dark">
+<?php
+   include("../core/loginmenu.php");
+?>
+<?php
+  include("../core/dropdownmenu.php");
+  
+?>
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-4 mt-5 bg-light rounded">
+    <div class="detail">
+      <div class=" bg-light rounded">
         <h1 class="text-center font-weight-bold text-primary">Contact Us</h1>
         <hr class="bg-light">
         <h5 class="text-center text-success"><?= $result;?></h5>
@@ -90,7 +112,19 @@ if(isset($_POST['submit'])){
       </div>
     </div>
   </div>
+<?php
+	include("../core/footer.php")
+?>
 </body>
 
 </html>
-</html>
+<?php
+	}else{
+		session_destroy();
+		//die();
+		//echo "<script language='javascript' type='text/javascript'> location.href='/smarthome/site/unauthorized/login.php' </script>";   
+		 header("Location: http://localhost/smarthome/site/unauthorized/login.php");
+		 exit();
+	}
+
+?>
